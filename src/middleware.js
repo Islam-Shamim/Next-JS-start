f3a1eaaa6f8ff5abcaa7c7f531cdba2b4b5da24a
Islam-Shamim <islamshamim1819@gmail.com>
2025-01-26 03:15:01 +0600

    ten

--- src/middleware.js
+++ src/middleware.js
@@ -1,12 +1,13 @@
 import { NextResponse } from "next/server"
 // const user = false
-const user = true
+// const user = true
 export const middleware = (request) => {
     // return NextResponse.redirect(new URL("/blogs/1", request.url)) //directly goes to new url by clicking matcher api
     // if (request.nextUrl.pathname.startsWith('/about')) {
     //     return NextResponse.rewrite(new URL("/blogs", request.url)) //same as redirect by it can't show hosting path  
     // }
-    if (!user) {
+    const cookies = request.cookies.get('token')
+    if (!cookies) {
         return NextResponse.redirect(new URL('/signIn', request.url))
     }
     return NextResponse.next() 
