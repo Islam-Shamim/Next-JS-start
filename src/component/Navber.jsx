cf750aa12021bf8bd690d27aac15161e40372d91
Islam-Shamim <islamshamim1819@gmail.com>
2025-02-03 14:18:01 +0600

    nothing

--- src/component/Navber.jsx
+++ src/component/Navber.jsx
@@ -1,44 +1,44 @@
 "use client"
 import Link from 'next/link'
-import {usePathname, useRouter } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 import React from 'react'
 export default function Navber() {
   const pathName = usePathname();
   const router = useRouter();
   const links = [
     {
-      title : "Home",
-      path : "/"
+      title: "Home",
+      path: "/"
     },
     {
-      title : "About",
-      path : "/about"
+      title: "About",
+      path: "/about"
     },
     {
-      title : "Blogs",
-      path : "/blogs"
+      title: "Blogs",
+      path: "/blogs"
     },
     {
-      title : "Contact",
-      path : "/contact"
+      title: "Contact",
+      path: "/contact"
     }
   ]
-  const handler = () =>{
+  const handler = () => {
     router.push("/signIn")
   }
   if (pathName.includes("/about"))
     return (
       <div className='flex justify-between items-center bg-slate-500 text-pink-500 text-2xl font-semibold'>
         <div className='text-4xl text-center p-4'>Welcome to <span className='text-cyan-200'>our Website</span></div>
-        <button className='bg-red-500 p-2 text-white rounded'><Link className='mr-2' href="/home">Home</Link></button>
+        <button className='bg-red-500 px-2 py-1 text-white rounded mr-4'><Link href="/home">Home</Link></button>
       </div>
     )
   return (
-    <div  className='sticky top-0 bg-yellow-300 flex justify-between items-center p-4'>
+    <div className='sticky top-0 bg-yellow-300 flex justify-between items-center p-4'>
       <h1 className='text-4xl font-semibold'>Next <span className='text-red-500'>Hero</span> </h1>
       <ul className='flex justify-between items-center space-x-6 text-xl font-semibold rounded'>
         {
-          links?.map((link)=><Link className={`${pathName === link.path && "hover:text-red-600 hover:bg-slate-300 rounded p-2"}`} key={link.path} href={link.path}>{link.title}</Link>)
+          links?.map((link) => <Link className={`${pathName === link.path && "hover:text-red-600 hover:bg-slate-300 rounded p-2"}`} key={link.path} href={link.path}>{link.title}</Link>)
         }
       </ul>
       <button onClick={handler} className='bg-cyan-500 rounded p-2'>Sign In</button>
