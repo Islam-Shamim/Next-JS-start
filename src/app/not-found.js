caafb79e83e9e78b0ccecc78999eaba610502a2f
Islam-Shamim <islamshamim1819@gmail.com>
2025-02-03 12:08:33 +0600

    not-found added

---
+++ src/app/not-found.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <div>
+        <h2 className="text-4xl font-bold text-center">404 - Page Not Found</h2>
+        <p className="text-center">Go back to the <Link href={'/home'}>Go Back Home</Link></p>
+    </div>
+  )
+}
